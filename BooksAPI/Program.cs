10ee33576006433a10d97edd186fdc27fca86620
ImDev11 <euris110811@gmail.com>
2023-10-21 01:56:23 -0400

    BookService created

--- BooksAPI/Program.cs
+++ BooksAPI/Program.cs
@@ -1,3 +1,6 @@
+﻿using BooksAPI.IServices;
+using BooksAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IBookService, BookService>();
 
 var app = builder.Build();
 
