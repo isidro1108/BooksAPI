bf528045f3d245ece2795d032fa84eab853f60bd
ImDev11 <euris110811@gmail.com>
2023-10-21 01:58:29 -0400

    feat: CRUD done

---
+++ BooksAPI/Controllers/BookController.cs
@@ -0,0 +1,98 @@
+﻿using BooksAPI.IServices;
+using BooksAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        protected readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var books = await _bookService.GetAll();
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Exception(ex.Message));
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Book book)
+        {
+            try
+            {
+                var newBook = await _bookService.Create(book);
+                return Ok(newBook);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Exception(ex.Message));
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var newBook = await _bookService.GetById(id);
+                if (newBook == null) 
+                    return NotFound();
+                return Ok(newBook);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Exception(ex.Message));
+            }
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update(int id, Book book)
+        {
+            try
+            {
+                if (book.Id == id)
+                {
+                    var bookUpdated = await _bookService.Update(book);
+                    return Ok(bookUpdated);
+                }
+                return BadRequest(new Exception("The id does not correspond to the book you want to update"));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Exception(ex.Message));
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _bookService.Delete(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Exception(ex.Message));
+            }
+        }
+    }
+}
